55486b82cb73d775b08341aaef1b3dd5720ed9e1
michellevo9 <michelle.vo109@gmail.com>
2023-10-10 11:46:44 -0500

    registration
    registration

---
+++ Spots-On/colors.js
@@ -0,0 +1,13 @@
+const COLORS = {
+  white: "#FFFFFF",
+  black: "#222222",
+  red: "F94144",
+  orange: "F3722C",
+  lightorange: "F8961E",
+  yellow: "F9C74F",
+  green: "90BE6D",
+  turquoise: "43AA8B",
+  blue: "577590",
+};
+
+export default COLORS;
