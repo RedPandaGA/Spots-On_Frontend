d150df02cbc63129957c49bc7b4c788d84f3ad8f
CulltheCake <110883605+CulltheCake@users.noreply.github.com>
2023-10-16 16:48:30 -0500

    Revert "added bottom navbar"
    This reverts commit 7a2ae28707609a4eacbc12864f837fe299553bd2.

--- Spots-On/App.js
+++ Spots-On/App.js
@@ -1,32 +1,20 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-import * as React from 'react';
-import MainContainer from './navigation/MainContainer';
-
-
-function App() {
-  return(
-    <MainContainer/>
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
