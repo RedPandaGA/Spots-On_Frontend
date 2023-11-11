f3176f90e783a28ded6d2542403ee0fae71060c5
Milan Nguyen <m.nguyen2035@gmail.com>
2023-11-11 13:18:45 -0600

    edited colors

--- Spots-On/components/colors.js
+++ Spots-On/components/colors.js
@@ -1,13 +1,9 @@
 const COLORS = {
   white: "#FFFFFF",
   black: "#222222",
-  red: "F94144",
-  orange: "F3722C",
-  lightorange: "F8961E",
-  yellow: "F9C74F",
-  green: "90BE6D",
-  turquoise: "43AA8B",
-  blue: "577590",
+  lightgreen: "#E7EFCA",
+  darkgreen: "#2C6765",
+  gold: "#D5B747"
 };
 
 export default COLORS;
