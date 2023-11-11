09aca3ddda6fa21b9e58db4a1f7b70a8b891229f
Milan Nguyen <m.nguyen2035@gmail.com>
2023-11-11 13:37:07 -0600

    basic login screen created

--- Spots-On/App.js
+++ Spots-On/App.js
@@ -4,13 +4,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainMap from './pages/mainMap';
 import Settings from './pages/settings';
+import Login from './pages/login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen 
+          name='Login' 
+          component={Login} 
+          options={{ headerShown: false}}
+        />
         <Stack.Screen 
           name='Home' 
           component={MainMap} 
