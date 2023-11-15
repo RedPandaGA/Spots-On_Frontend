1c65be84e0b22f58939f43e6f3c11d91869651b2
Faris Khattak <fariskhattak8@gmail.com>
2023-11-15 15:19:07 -0600

    Fixed android map button shadow

--- Spots-On/components/mapButton.js
+++ Spots-On/components/mapButton.js
@@ -1,44 +1,53 @@
 import React from "react";
-import { StyleSheet, Image, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, View, TouchableOpacity } from "react-native";
 
-export default function MapButton({ imageSource, onPress, style, width, height, active }) {
-    let backgroundColor = 'rgba(44, 103, 101, 1)';
-    if (active) {
-        backgroundColor = 'rgba(255, 85, 85, 1)'
-    }
-    
-    return (
-        <View style={[style, styles.shadow]}>
-            <TouchableOpacity onPress={onPress} >
-                <View style={[styles.button, {width: width, height: height, backgroundColor: backgroundColor}]}>
-                    <Image 
-                        source={imageSource}
-                        style={styles.image}
-                    />
-                </View>
-            </TouchableOpacity>
+export default function MapButton({
+  imageSource,
+  onPress,
+  style,
+  width,
+  height,
+  active,
+}) {
+  let backgroundColor = "rgba(44, 103, 101, 1)";
+  if (active) {
+    backgroundColor = "rgba(255, 85, 85, 1)";
+  }
+
+  return (
+    <View style={[style, styles.shadow]}>
+      <TouchableOpacity onPress={onPress}>
+        <View
+          style={[
+            styles.button,
+            { width: width, height: height, backgroundColor: backgroundColor },
+          ]}
+        >
+          <Image source={imageSource} style={styles.image} />
         </View>
-    )
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 50,
-        paddingVertical: 14,
-        paddingHorizontal: 10,
-    },
-    image: {
-        width: 35,
-        height: 35,
-    },
-    shadow: {
-        elevation: 2,
-        shadowColor: '#171717',
-        shadowOffset: {width: 0, height: 0},
-        shadowOpacity: .6,
-        shadowRadius: 10,
-        borderRadius: 50,
-    },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    paddingVertical: 14,
+    paddingHorizontal: 10,
+  },
+  image: {
+    width: 35,
+    height: 35,
+  },
+  shadow: {
+    elevation: 20,
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.6,
+    shadowRadius: 10,
+    borderRadius: 50,
+  },
 });
