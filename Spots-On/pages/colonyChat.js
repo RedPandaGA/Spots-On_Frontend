fc4c7429b2f58f7e73c154a6cdd2e6602645a6c5
Milan Nguyen <m.nguyen2035@gmail.com>
2023-11-15 14:13:02 -0600

    basic chat sending stuff :D

--- Spots-On/pages/colonyChat.js
+++ Spots-On/pages/colonyChat.js
@@ -1,7 +1,33 @@
-import React, { useState }  from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { View, StyleSheet, Image, TouchableOpacity, TextInput, Text } from 'react-native';
+
 export default function ColonyChat({ navigation }) {
-  const [chat, setChat] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const onSend = () => {
+    if (inputText.trim() === '') {
+      return;
+    }
+
+    // Add the user's message to the chat
+    const newMessages = [
+      {
+        _id: messages.length + 1,
+        text: inputText,
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'User', // You can set the user's name dynamically if needed
+        },
+      },
+    ];
+
+    setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
+    setInputText('');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -29,68 +55,83 @@ export default function ColonyChat({ navigation }) {
           </View>
         </TouchableOpacity>
       </View>
-      <TextInput  
-        style={styles.input}
-        // placeholder=""
-        placeholderTextColor={"#E7EFCA"}
-        value={chat}
-        onChangeText={(text) => setChat(text)}
+      <GiftedChat
+        messages={messages}
+        onSend={(newMessages) => onSend(newMessages)}
+        user={{ _id: 1 }}
+        renderInputToolbar={(props) => {
+          // Optional: Customize the input toolbar
+          return (
+            <View style={styles.inputToolbarContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Type your message..."
+                placeholderTextColor="#E7EFCA"
+                value={inputText}
+                onChangeText={(text) => setInputText(text)}
+              />
+              <TouchableOpacity onPress={() => onSend()} style={styles.sendButton}>
+                <Text style={styles.sendButtonText}>Send</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
       />
     </View>
   );
 }
+
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#2C6765',
-    },
-    header: {
-      marginTop: 50,
-    },
-    title: {
-      color: '#E7EFCA',
-      fontSize: 35,
-      textAlign: 'center',
-      fontWeight: 'bold',
-      width: '100%',
-      paddingLeft: '3%',
-      alignSelf: 'center'
-    }, 
-    subtitle: {
-      color: '#D5B747',
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginLeft: 20,
-      marginBottom: -15,
-      paddingTop: 20,
-      opacity: .9
-    }, 
-    image: {
-      height: 50,
-      width: 50,
-      position: 'absolute',
-      left: 20,
-    },
-    threeDots: {
-      height: 40,
-      width: 40,
-      position: 'absolute',
-      top: -20,
-      right: 10,
-    },
-    input: {
-      height: 40,
-      width: '90%',
-      borderColor: '#305c5c',
-      borderWidth: 2,
-      marginTop: '193%',
-      margin: '5%',
-      borderRadius: 30,
-      color: "#E7EFCA",
-      backgroundColor: "#305c5c",
-      fontWeight: 'bold',
-      fontSize: 16,
-      textAlign: 'center'
-    },
-    
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#2C6765',
+  },
+  header: {
+    marginTop: 50,
+  },
+  title: {
+    color: '#E7EFCA',
+    fontSize: 35,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    width: '100%',
+    paddingLeft: '3%',
+    alignSelf: 'center'
+  }, 
+  image: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    left: 20,
+  },
+  threeDots: {
+    height: 40,
+    width: 40,
+    position: 'absolute',
+    top: -20,
+    right: 10,
+  },
+  inputToolbarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    backgroundColor: '#305c5c',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    color: '#E7EFCA',
+    backgroundColor: '#305c5c',
+  },
+  sendButton: {
+    marginLeft: 10,
+  },
+  sendButtonText: {
+    color: '#E7EFCA',
+    fontWeight: 'bold',
+  },
+});
